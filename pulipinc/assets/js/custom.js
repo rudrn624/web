7f559e4fdebe8f9436970cdfb61e05dd053c0add
뱅골라이언 <rudrn624@naver.com>
2020-05-31 01:13:58 +0900

    20200531

--- pulipinc/assets/js/custom.js
+++ pulipinc/assets/js/custom.js
@@ -1,19 +1,4 @@
- var mySwiper = new Swiper ('#headSlider .swiper-container', {
-    // Optional parameters
-    loop: true,
-    slidePerview : 1,
-    // If we need pagination
-    pagination: {
-      el: '#headSlider .swiper-pagination',
-      clickable : 'true',
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '#headSlider .swiper-button-next',
-      prevEl: '#headSlider .swiper-button-prev',
-    },
-    autoplay: {
-      delay: 5000,
-    },
-  });
+var swiper = new Swiper('.serviceList.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 40,
+});
