32179d022c767a574dd0ade48dfeed88d763d9c8
뱅골라이언 <rudrn624@naver.com>
2020-05-31 16:27:25 +0900

    20200531

--- pulipinc/assets/js/custom.js
+++ pulipinc/assets/js/custom.js
@@ -2,3 +2,12 @@ var swiper = new Swiper('.serviceList.swiper-container', {
   slidesPerView: 3,
   spaceBetween: 40,
 });
+var swiper1 = new Swiper('.slideContentinner .swiper-container', {
+  slidesPerView: 'auto',
+  spaceBetween: 40,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+});
